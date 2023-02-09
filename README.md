3cb2a5e6d7ac808d2fd2986ee466add6826c0d3c
Jacob Postill <postilljacob@gmail.com>
2023-02-09 16:48:04 -0400

    working on licsences button

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Module-9-Challenge
+# sadfg
+
+[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt) 
+    
+## Description
+
+sadfg
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+
+## Installation
+
+asdgf
+
+## Usage 
+
+asdf
+
+## License
+
+Apache License 2.0
+
+## Contributing
+
+asdf
+
+## Tests
+
+asdf
+
+## Questions
+
+asdf
---
+++ script.js
@@ -0,0 +1,113 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+var profile = new Promise(function (resolve, reject) {
+    resolve(inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please provide a description for your project."
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please provide the installation for your project."
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide the usage of your project."
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Please select the license for your project.",
+            choices: [
+                "Apache License 2.0",
+                "GNU General Public License v3.0",
+                "MIT License",
+                'BSD 2-Clause "Simplified" License',
+                'BSD 3-Clause "New" or "Revised" License',
+                'Boost Software License 1.0',
+                'Creative Commons Zero v1.0 Universal',
+                'Eclipse Public License 2.0',
+                'GNU Affero General Public License v3.0',
+                'GNU General Public License v2.0',
+                'GNU Lesser General Public License v2.1',
+                'Mozilla Public License 2.0',
+                'The Unlicense',
+            ]
+        },
+        {
+            type: "input",
+            name: "contribute",
+            message: "Please provide any contributions guidlines to your project."
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "Please provide the test instructions for your project."
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "Please provide any qestions for your project."
+        },
+    ])
+    )
+});
+
+profile.then(function(data){
+    const readme = `${generateMarkdown(data)}
+    
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+
+## Installation
+
+${data.installation}
+
+## Usage 
+
+${data.usage}
+
+## License
+
+This application is covered by the following license: ${data.license}.
+
+## Contributing
+
+${data.contribute}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+${data.questions}
+`;
+    fs.writeFile('README.md', readme, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log('README has been created!');
+    });
+})
